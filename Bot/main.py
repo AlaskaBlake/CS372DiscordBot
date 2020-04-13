46a7adabe0124a0b2b847227126f3226b77eaf9a
AlaskaBlake <59948466+AlaskaBlake@users.noreply.github.com>
2020-04-12 20:16:13 -0800

    token updated

--- Bot/main.py
+++ Bot/main.py
@@ -3,4 +3,4 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="~")
 
-client.run('Njk5MDg2MDM2MjUyMjI5NjQy.XpPQpA.dcmXleCtfTWEHTjR4KiX-AOLUS0')
+client.run('Njk5MDg2MDM2MjUyMjI5NjQy.XpPmSA.1ZGeBLqs18SLt98ehCw67Gr80BU')
