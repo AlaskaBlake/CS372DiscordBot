6ad6f3468e0c5be228514f42e97078a712a39a66
AlaskaBlake <59948466+AlaskaBlake@users.noreply.github.com>
2020-04-15 16:28:22 -0800

    commit

--- Bot/main.py
+++ Bot/main.py
@@ -1,6 +1,29 @@
 import discord
+import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="~")
 
-client.run('Njk5MDg2MDM2MjUyMjI5NjQy.XpPQpA.dcmXleCtfTWEHTjR4KiX-AOLUS0')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswitch('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+# This is not the Bots actual token, contributors have the correct token.
+client.run('Njk5MDg2MDM2MjUyMjI5NjQy.XpPnmQ.Mva-ffJWClpKBefvVJCYqUBO1Ps')
