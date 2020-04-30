2d26e3db435a2e59df6bf1d634977e9624e26777
willis240 <willingness240@gmail.com>
2020-04-30 14:59:12 -0800

    audio.py plays audio, still doesn't work with other files yet

--- Bot/cogs/audio.py
+++ Bot/cogs/audio.py
@@ -1,21 +1,43 @@
 import discord
+from discord.ext import commands
 import configparser
+
 config = configparser.ConfigParser()
-config.sections()
+
 config.read('config.ini')
 
 KEY = config['JAKE_SERVER']['BOT_KEY']
-client = discord.Client()
+bot = commands.Bot(command_prefix = '.')
 
-@client.event
+players = {}
+
+@bot.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    await ctx.bot.disconnect()
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord! No, no, please, no!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
-client.run(str(KEY))
+@bot.command(pass_context=True)
+async def play(ctx):
+    guild = ctx.guild
+    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio('Oh_No.mp3')
+    if not voice_client.is_playing():
+        voice_client.play(audio_source, after=None)
+
+
+bot.run(KEY)
