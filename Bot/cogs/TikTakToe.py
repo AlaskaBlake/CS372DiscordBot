0c40ff8525620def73e0536e9d7512d9704fadcc
Keegan Palmieri <47491011+fkpalmieri@users.noreply.github.com>
2020-05-03 23:07:22 -0800

    update
    create board requires update regarding second command integration

--- Bot/cogs/TikTakToe.py
+++ Bot/cogs/TikTakToe.py
@@ -2,56 +2,6 @@ import discord
 from discord.ext import commands
 
 
-class TikTakToe(commands.Cog):
-    def __init__(self, client):
-        self.client = client
-
-    @commands.command()
-    async def TikTakToe(self, ctx):
-        await ctx.send('Welcome to Tic-Tac-Toe!')
-
-
-def setup(client):
-    client.add_cog(TikTakToe(client))
-
-##
-##
-##  Beginning of TicTaKToe Intagration
-##
-##
-
-def drawBoard(board):
-    # This function prints out the board that it was passed.
-
-    # "board" is a list of 10 strings representing the board (ignore index 0)
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-
-
-def inputPlayerLetter():
-    # Lets the player type which letter they want to be.
-    # Returns a list with the player's letter as the first item, and the computer's letter as the second.
-    letter = ''
-    while not (letter == 'X' or letter == 'O'):
-        print('Do you want to be X or O?')
-        letter = input().upper()
-
-    # the first element in the tuple is the player's letter, the second is the computer's letter.
-    if letter == 'X':
-        return ['X', 'O']
-    else:
-        return ['O', 'X']
-
-
 def playAgain():
     # This function returns True if the player wants to play again, otherwise it returns False.
     print('Do you want to play again? (yes or no)')
@@ -60,6 +10,16 @@ def playAgain():
 
 def makeMove(board, letter, move):
     board[move] = letter
+
+
+async def getPlayerMove(board):
+    # Let the player type in his move.
+    move = ' '
+    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+        print("Whats your move")
+        await TikTakToe.move
+        move = TikTakToe.move
+    return int(move)
 
 
 def isWinner(bo, le):
@@ -90,54 +50,79 @@ def isSpaceFree(board, move):
     return board[move] == ' '
 
 
-def getPlayerMove(board):
-    # Let the player type in his move.
-    move = ' '
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
-        print('What is your next move? (1-9)')
-        move = input()
-    return int(move)
-
-
-
-
 def isBoardFull(board):
     # Return True if every space on the board has been taken. Otherwise return False.
     for i in range(1, 10):
-        if isSpaceFree(board, i):
+        if board.isSpaceFree(board, i):
             return False
     return True
 
 
-print('Welcome to Tic Tac Toe!')
+class TikTakToe(commands.Cog):
+    def __init__(self, client):
+        self.client = client
 
-while True:
-    # Reset the board
-    theBoard = [' '] * 10
-    playerLetter, computerLetter = inputPlayerLetter()
+    @commands.command()
+    async def move(self, ctx, move):
+        await ctx.send("What's your move?")
+        return move
 
-    print('The ' + turn + ' will go first.')
-    gameIsPlaying = True
+    @commands.command()
+    async def tictactoe(self, ctx):
+        while True:
+            # Reset the board
+            theBoard = [' '] * 10
+            player1, player2 = ['X', 'O']
+            turn = 'player_x'
+            await ctx.send('The "X" will go first.')
+            gameIsPlaying = True
 
-    while gameIsPlaying:
-        if turn == 'player':
-            # Player's turn.
-            drawBoard(theBoard)
-            move = getPlayerMove(theBoard)
-            makeMove(theBoard, playerLetter, move)
+            while gameIsPlaying:
+                if turn == 'player_x':
+                    # Player's turn.
 
-            if isWinner(theBoard, playerLetter):
-                drawBoard(theBoard)
-                print('Hooray! You have won the game!')
-                gameIsPlaying = False
-            else:
-                if isBoardFull(theBoard):
-                    drawBoard(theBoard)
-                    print('The game is a tie!')
-                    break
+                    await ctx.send('~' + theBoard[7] + '~|~' + theBoard[8] + '~|~' + theBoard[9] + '~')
+                    await ctx.send('----------')
+                    await ctx.send('~' + theBoard[4] + '~|~' + theBoard[5] + '~|~' + theBoard[6] + '~')
+                    await ctx.send('----------')
+                    await ctx.send('~' + theBoard[1] + '~|~' + theBoard[2] + '~|~' + theBoard[3] + '~')
+
+                    move = getPlayerMove(theBoard)
+                    makeMove(theBoard, player1, move)
+
+                    if isWinner(theBoard, player1):
+                        # drawBoard(theBoard)
+                        await ctx.send('Hooray! X has won the game!')
+                        gameIsPlaying = False
+                    else:
+                        if isBoardFull(theBoard):
+                            # drawBoard(theBoard)
+                            await ctx.send('The game is a tie!')
+                            break
+                        else:
+                            turn = 'player_o'
+
                 else:
-                    turn = 'computer'
+                    # Player's turn.
+                    # drawBoard(theBoard)
+                    move = getPlayerMove(theBoard)
+                    makeMove(theBoard, player2, move)
+
+                    if isWinner(theBoard, player2):
+                        # drawBoard(theBoard)
+                        await ctx.send('Hooray! O has won the game!')
+                        gameIsPlaying = False
+                    else:
+                        if isBoardFull(theBoard):
+                            # drawBoard(theBoard)
+                            await ctx.send('The game is a tie!')
+                            break
+                        else:
+                            turn = 'player_x'
+
+            if not playAgain():
+                break
 
 
-    if not playAgain():
-        break
+def setup(client):
+    client.add_cog(TikTakToe(client))
