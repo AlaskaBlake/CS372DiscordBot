70ede306a9ad42db39d18d4c7298664da020de49
mchychym <60200816+mchychym@users.noreply.github.com>
2020-05-03 16:53:39 -0800

    Final commit
    needed for animal.py

---
+++ Bot/cogs/embedlinks.py
@@ -0,0 +1,33 @@
+catLinks = [
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cat-named-dymka-in-the-veterinary-clinic-best-where-it-had-news-photo-1582304174.jpg",
+    "https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png",
+    "https://media.npr.org/assets/img/2019/08/15/hank_wide-4df45b477add6d4b17d8e89e29a7572ba3dadc40.jpg",
+    "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/03/Russian-Blue_01.jpg",
+    "https://cdn.mos.cms.futurecdn.net/otjbibjaAbiifyN9uVaZyL-320-80.jpg",
+    "https://wpcdn.us-east-1.vip.tn-cloud.net/www.channel3000.com/content/uploads/2020/04/cat.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtVfQZycBRHV3nMxhB_r6iCanq8LKOD0fKwEwUzYASXU9yqF2z&usqp",
+    "https://api.time.com/wp-content/uploads/2015/02/cats.jpg"]
+
+dogLinks = [
+    "https://3snrvh1nlkatbz4yq1mpe7la-wpengine.netdna-ssl.com/wp-content/uploads/2011/07/cute-puppy-dog-wallpapers.jpg",
+    "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/292/292567/bichon-frise-dog.jpg",
+    "https://i.ytimg.com/vi/Vp7nW2SP6H8/maxresdefault.jpg"
+    "https://i.guim.co.uk/img/media/20098ae982d6b3ba4d70ede3ef9b8f79ab1205ce/0_0_969_1005/master/969.jpg",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/large-dog-breeds-lead-1550810849.jpg"]
+
+surpriseLinks = ["https://www.randomlists.com/img/animals/mink.webp",
+                 "https://www.randomlists.com/img/animals/lovebird.webp",
+                 "https://lh3.googleusercontent.com/proxy/8pPMxz6lV_NJNEhubaDxA3yINoe1IRknUm_8QQgGDc_qBA1J6XvDU9w5xG9EWCmUw_8SesIF4EOFy1_8xzMNBqghVkfgNt4M0zbovYYBvFQbPDPgWPl7tMgjmMQ5cMvoiNpeynwrxxFDt4cjxqlrT-w",
+                 "https://i.imgur.com/rrSbldT.jpg?1",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvR1ltRdBDuWsMDI823bPPuYjJTfUbSV5TfK8vfJWIOEmIqiSr&usqp=CAU",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTt7BpOMO6PynsaKXGp_7ZaC0dx4aU4k60sd5KaN4PQqQ-OzVum&usqp=CAU",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyn7hpRNRuvYffJHv0fcnKf-oIfF1d9SCSGr1uNuoooCx0WNLS&usqp=CAU",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWRgILpt4Q5wkkznkA8QWLYJPZOHaJXKS5x5ZIsnACTNxNXNjh&usqp=CAU"
+                 "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cat-named-dymka-in-the-veterinary-clinic-best-where-it-had-news-photo-1582304174.jpg",
+                 "https://cdn.mos.cms.futurecdn.net/otjbibjaAbiifyN9uVaZyL-320-80.jpg",
+                 "https://wpcdn.us-east-1.vip.tn-cloud.net/www.channel3000.com/content/uploads/2020/04/cat.jpg",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtVfQZycBRHV3nMxhB_r6iCanq8LKOD0fKwEwUzYASXU9yqF2z&usqp",
+                 "https://api.time.com/wp-content/uploads/2015/02/cats.jpg",
+                 "https://3snrvh1nlkatbz4yq1mpe7la-wpengine.netdna-ssl.com/wp-content/uploads/2011/07/cute-puppy-dog-wallpapers.jpg",
+                 "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/292/292567/bichon-frise-dog.jpg",
+                 "https://i.ytimg.com/vi/Vp7nW2SP6H8/maxresdefault.jpg"]
